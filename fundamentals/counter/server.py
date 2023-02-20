2332c728f53eacaed106146c39ea1924590429c7
n3ndor <n3ndor@gmail.com>
2023-02-19 22:48:31 -0300

    added static and templates

--- fundamentals/counter/server.py
+++ fundamentals/counter/server.py
@@ -1,1 +1,14 @@
-import flask from Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/destroy_session")
+def destroy():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
