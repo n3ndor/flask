a1266b4f1502542ff8ff5456748c7ce978a1a924
n3ndor <n3ndor@gmail.com>
2023-02-24 13:40:58 -0300

    new flask project

--- fundamentals/ninja_gold/server.py
+++ fundamentals/ninja_gold/server.py
@@ -1,21 +1,25 @@
 from flask import Flask, render_template, redirect, session, request
+import random
 
 app = Flask(__name__)
 
 app.secret_key="we are going to be ninjas again"
 
-
-
 @app.route("/")
 def index():
     gold = 0
+    if "gold" not in session:
+        session["gold"] = 0
+
     return render_template("index.html", gold=gold)
 
 @app.route("/process_money", methods=["POST"])
 def calculating():
     
-    
+    print(session["gold"])
     return redirect("index.html")
+    
+    # return redirect("/")
 
 
 
